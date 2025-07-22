4bd37f94fbc57d86a2ca3db9be056c149d474e99
6arshid <51836304+6arshid@users.noreply.github.com>
2025-07-23 01:39:21 +0200

    Fix seeder unique constraint

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
@@ -20,6 +21,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class Order extends Model
 {
+    use HasFactory;
     protected $casts = [
         'items' => 'array',
         'shipping_info' => 'array',
