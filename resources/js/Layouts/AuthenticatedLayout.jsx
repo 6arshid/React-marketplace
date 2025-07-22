98508550f3b282f56c4662e820b58dee5d457cb5
6arshid <51836304+6arshid@users.noreply.github.com>
2025-07-22 07:37:02 +0200

    Fix user dropdown alignment

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -100,7 +100,7 @@ export default function AuthenticatedLayout({ header, children }) {
                             <span className="inline-flex rounded-md">
                                 <button
                                     type="button"
-                                    className="flex items-center rounded-md border border-transparent px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+                                    className="flex w-full items-center justify-between rounded-md border border-transparent px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
                                 >
                                     {user.name}
                                     <svg
