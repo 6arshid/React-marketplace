4544d3b62c078934066cf72195cbb623d8a26a2d
6arshid <51836304+6arshid@users.noreply.github.com>
2025-07-25 01:02:42 +0200

    Remove profile views section from statistics page

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
-use App\Models\ViewStatistic;
+
 
 class StatisticsController extends Controller
 {
@@ -18,19 +18,8 @@ class StatisticsController extends Controller
             ->take(10)
             ->get(['id', 'title', 'slug', 'views']);
 
-        $startDate = now()->subDays(29)->startOfDay();
-        $dates = collect(range(0, 29))->map(fn ($i) => $startDate->copy()->addDays($i)->toDateString());
-        $stats = $user->viewStatistics()
-            ->whereBetween('date', [$dates->first(), $dates->last()])
-            ->get()
-            ->keyBy('date');
-
-        $profileViews = $dates->map(fn ($d) => $stats[$d]->profile_views ?? 0);
-
         return Inertia::render('Statistics', [
             'topProducts' => $topProducts,
-            'labels' => $dates,
-            'profileViews' => $profileViews,
         ]);
     }
 }
