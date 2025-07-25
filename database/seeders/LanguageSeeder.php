ddfbd72a1caf7d12919114716b24edd77257656c
Farshid Mahmoudi <51836304+6arshid@users.noreply.github.com>
2025-07-25 04:44:29 +0200

    Add language

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -16,11 +16,41 @@ class LanguageSeeder extends Seeder
                 'direction' => 'ltr',
                 'translations' => json_decode(file_get_contents(resource_path('js/locales/en/translation.json')), true),
             ],
+                        [
+                'name' => '中文',
+                'code' => 'zh',
+                'direction' => 'ltr',
+                'translations' => json_decode(file_get_contents(resource_path('js/locales/zh/translation.json')), true),
+            ],
+                                 [
+                'name' => 'हिन्दी',
+                'code' => 'hi',
+                'direction' => 'ltr',
+                'translations' => json_decode(file_get_contents(resource_path('js/locales/hi/translation.json')), true),
+            ],
+              [
+                'name' => 'Español',
+                'code' => 'es',
+                'direction' => 'ltr',
+                'translations' => json_decode(file_get_contents(resource_path('js/locales/es/translation.json')), true),
+            ],
+                          [
+                'name' => 'Français',
+                'code' => 'fr',
+                'direction' => 'ltr',
+                'translations' => json_decode(file_get_contents(resource_path('js/locales/fr/translation.json')), true),
+            ],
             [
-                'name' => 'Farsi',
+                'name' => 'فارسی',
                 'code' => 'fa',
                 'direction' => 'rtl',
                 'translations' => json_decode(file_get_contents(resource_path('js/locales/fa/translation.json')), true),
+            ],
+                        [
+                'name' => 'العربية',
+                'code' => 'ar',
+                'direction' => 'rtl',
+                'translations' => json_decode(file_get_contents(resource_path('js/locales/ar/translation.json')), true),
             ],
         ];
 
